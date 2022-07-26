b7e4fb483d2dde265a66f60049b482dadb468f1d
GisellaNerinaGonzalez <109997812+GisellaNerinaGonzalez@users.noreply.github.com>
2022-07-25 22:11:26 -0300

    first commit

---
+++ trabajo final.cs
@@ -0,0 +1,42 @@
+﻿// Aspectos a incluir
+//Clase usuario; PROPIEDADES: id, apellido,nombreUsuario,Contrasena, mail
+namespace Desafio1
+{
+    
+    
+     public class Usuario
+        {
+            private string id;
+            private string apellido;
+            private string nombreUsuario;
+            private string contraseña;
+            private string mail; 
+            
+        }
+    //si quiero que el cliente o usuario ponga todos los datos, se le crea una logica con get y set
+    
+    public class Producto//Clase producto: PROPIEDADES: id,descripcion, costo, precioVenta,stock,id usuario
+    {
+     private string id;
+     private string descripcion;
+     private double costo;
+     private double precioVenta;
+     private int stock;
+     private string idusuario;
+
+        
+    }
+    public class ProductoVendido// PROPIEDADES: id, idProducto, Stock, idVenta
+    {
+        private string id;
+        private string idProducto;
+        private int Stock;
+        private string idVenta;
+    }//hacer publico
+    public class Venta // Clase venta: PROPIEDADES: id, comentarios
+    {
+        private string id;
+        private string comentarios;
+    }
+}
+
